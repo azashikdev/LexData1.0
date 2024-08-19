e4038acbd02c1f1368e9f53519976d8eec1753da
ashik <azashikdev@gmaail.com>
2024-08-19 17:27:56 +0600

    init vite project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-1">
+          <h1>This is class 1</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
